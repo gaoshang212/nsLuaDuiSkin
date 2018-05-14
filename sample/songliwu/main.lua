32a89b8e9a39cb62158e45b88dfda53c424ae90f
gaoshang212 <gaoshang212@163.com>
2018-05-14 18:33:01 +0800

    add demo

--- sample/songliwu/main.lua
+++ sample/songliwu/main.lua
@@ -12,13 +12,19 @@ local app=UI.CApplicationUI.sharedInstance()
 local Window=UI.CWindowUI
 
 --app:setResourceZip("360Demo.zip")
-local wnd=Window.create({res="install.xml",name="yongxin",x=0,y=0,w=508,h=418})
+local wnd=Window.create({res="install.xml",name="songliwu",x=0,y=0,w=508,h=418, type="popup"})
 wnd:centerWindow()
 
 nsis = {
     onProgress = function(progress)
-        local control = wnd:findControl("install_progress")
+        local control = wnd:findControl("slrProgress")
         control:setValue(progress)
+
+        if(progress >= 100)
+        then
+            local tab = wnd:findControl("wizardTab")
+            tab:selectItem(2)
+        end
     end
 }
 
